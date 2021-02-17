f3fddca9441897bed2c61cbcb1e8d9c323758a80
Rabson J Phiri <phirirabsonj@gmail.com>
2021-02-17 18:35:00 +0100

    add SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { Component } from 'react';
+import './App.css';
 import Map from './components/Map';
+import SearchBar from './components/SearchBar';
 import WeatherCard from './components/WeatherCard';
-import './App.css'
 import WeatherDetail from './components/WeatherDetail';
 class App extends Component {
 	state = { weather: {} };
@@ -13,18 +14,19 @@ class App extends Component {
 			const { data } = await axios.get(
 				`https://api.openweathermap.org/data/2.5/weather?q=Casablanca&units=metric&appid=${key}`
 			);
-         console.log(data);
-         this.setState({weather: data})
+			console.log(data);
+			this.setState({ weather: data });
 		} catch (err) {
 			console.log(err);
-				console.log(process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY);
-
 		}
 	};
 
-   render() {
+	onSubmit = (term) => {}
+
+	render() {
 		return (
 			<div className="container">
+				<SearchBar onSubmit={this.onSubmit}/>
 				<WeatherCard weatherData={this.state.weather} />
 				<WeatherDetail weatherData={this.state.weather} />
 				<Map weatherData={this.state.weather} />
